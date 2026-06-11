5f04ea2ea531da81d687cbcf8bc5e73765648260
ngTanPhuc <nguyentanphuc020505@gmail.com>
2026-06-11 09:41:47 +0700

    Unzip the file and delete the old .tar.gz (Gosh this is such a handfull)

--- scripts/download_artifacts.sh
+++ scripts/download_artifacts.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-mkdir -p models/sam3
-
 kaggle datasets download -d ngtanphuc020505/land-parcel-boundary-delineation -p data/raw --unzip
 kaggle models instances versions download ngtanphuc020505/sam-3-pretrained/pytorch/default/1 -p models/pretrained/sam3
+
+tar -xzf models/pretrained/sam3/*.tar.gz -C models/pretrained/sam3
+rm models/pretrained/sam3/*.tar.gz
