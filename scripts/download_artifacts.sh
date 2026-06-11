19123b43daa15981569a87fbc2eca35797719561
ngTanPhuc <nguyentanphuc020505@gmail.com>
2026-06-11 08:43:50 +0700

    Fix the download model command

--- scripts/download_artifacts.sh
+++ scripts/download_artifacts.sh
@@ -4,4 +4,4 @@ mkdir -p data/raw
 mkdir -p models/pretrained
 
 kaggle datasets download -d ngtanphuc020505/land-parcel-boundary-delineation -p data/raw --unzip
-kaggle datasets download -d ngtanphuc020505/sam-3-pretrained -p models/pretrained --unzip
+kaggle models instances download ngtanphuc020505/sam-3-pretrained/pytorch/sam3-h/1 -p models/pretrained --unzip
